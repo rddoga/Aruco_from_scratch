9656150589d7e0068baf32268d08e9d3a8166461
Axel Barbier <barbieraxel@gmail.com>
2022-05-10 13:50:48 +0200

    markermap and tool tip position

---
+++ aruco_3.1.15/Custom/test_MarkerMap.cpp
@@ -0,0 +1,23 @@
+#include "v4l2_helper.h"
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include "aruco.h"
+
+#include <vector>
+//#include <tuple>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <stdexcept>
+
+using namespace std;
+
+
+int imgSizeX = 1920, imgSizeY = 1080;
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
